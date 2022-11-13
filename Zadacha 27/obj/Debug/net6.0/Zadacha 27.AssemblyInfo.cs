5ade9390fdbe07eaabdd8bed06a3b1e658bdb90d
ElenaRudakova <er171173@gmail.com>
2022-11-13 12:52:32 +0300

    Добавить Задачу 27

--- Zadacha 27/obj/Debug/net6.0/Zadacha 27.AssemblyInfo.cs
+++ Zadacha 27/obj/Debug/net6.0/Zadacha 27.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Seminar 4")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Zadacha 27")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Seminar 4")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Seminar 4")]
+[assembly: System.Reflection.AssemblyProductAttribute("Zadacha 27")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Zadacha 27")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
